b7340650aa7fb2993f7db361fee162c204bcc8a6
SUKHAD SHARMA <94107940+SCORLEOs773@users.noreply.github.com>
2024-01-01 11:54:56 +0530

    First Commit
    Added initial Files

---
+++ prithvi.js
@@ -0,0 +1,250 @@
+document.querySelector(".english").addEventListener("click", function() {
+    document.querySelector(".how").textContent = "HOW TO USE PRITHVI 1.0";
+
+    document.querySelector(".p1").textContent =
+        "PRITHVI 1.0 is an indegeneous built system which helps you to uniquely and accurately calculate the best Loam Conservation method which best suits your land.";
+
+    document.querySelector(".p2").textContent =
+        "Just enter the required parameters and PRITHVI will automatically suggest you multiple methods which you can follow in order to reduce soil erosion in your land.";
+
+    document.querySelector(".english").classList.add("active-page");
+    document.querySelector(".hindi").classList.remove("active-page");
+    document.querySelector(".punjabi").classList.remove("active-page");
+
+    document.querySelector(".title").textContent = "PRITHVI 1.0";
+    document.querySelector(".dropdownheader").textContent =
+        "Please select the appropriate parameters -";
+    document.querySelector(".state").textContent = "STATE -";
+    document.querySelector(".region").textContent = "REGION -";
+    document.querySelector(".soil").textContent = "TYPE OF SOIL -";
+    document.querySelector(".rain").textContent = "ANNUAL RAINFALL -";
+    document.querySelector(".terrain").textContent = "TERRAIN -";
+    document.querySelector(".groundWater").textContent =
+        "GROUND WATER LEVEL (in Meters Below Ground Level(Min - Max)) -";
+    document.querySelector(".submit").value = "SUBMIT";
+
+    document.querySelector(".pun2").textContent = "Punjab";
+
+    document.querySelector(".pun").textContent = "Punjab";
+    document.querySelector(".up").textContent = "Uttar Pradesh";
+    document.querySelector(".wb").textContent = "West Bengal";
+    document.querySelector(".guj").textContent = "Gujarat";
+    document.querySelector(".har").textContent = "Haryana";
+    document.querySelector(".mp").textContent = "Madhya Pradesh";
+    document.querySelector(".assa").textContent = "Assam";
+    document.querySelector(".ap").textContent = "Andhra Pradesh";
+    document.querySelector(".kar").textContent = "Karnataka";
+    document.querySelector(".chha").textContent = "Chhatisgrah";
+
+    document.querySelector(".sandy").textContent = "Sandy Soil";
+    document.querySelector(".silt").textContent = "Silt Soil";
+    document.querySelector(".clay").textContent = "Clay Soil";
+    document.querySelector(".loamy").textContent = "Loamy Soil";
+    document.querySelector(".red").textContent = "Red Soil";
+    document.querySelector(".black").textContent = "Black Soil";
+    document.querySelector(".alluvial").textContent = "Alluvial Soil";
+    document.querySelector(".peaty").textContent = "Peaty Soil";
+    document.querySelector(".saline").textContent = "Saline Soil";
+    document.querySelector(".chalky").textContent = "Chalky Soil";
+    document.querySelector(".other").textContent = "Other ...";
+
+    document.querySelector(".u20").textContent = "Under 20 cm";
+    document.querySelector(".n2040").textContent = "20 - 40 cm";
+    document.querySelector(".n4060").textContent = "40 - 60 cm";
+    document.querySelector(".n60100").textContent = "60 - 100 cm";
+    document.querySelector(".n100150").textContent = "100 - 150 cm";
+    document.querySelector(".n150200").textContent = "150 - 200 cm";
+    document.querySelector(".o200").textContent = "Over 200 cm";
+
+    document.querySelector(".plat").textContent = "Plateau";
+    document.querySelector(".moun").textContent = "Mountain";
+    document.querySelector(".pla").textContent = "Plains";
+    document.querySelector(".val").textContent = "Valley";
+    document.querySelector(".tun").textContent = "Tundra";
+    document.querySelector(".oas").textContent = "Oasis";
+    document.querySelector(".des").textContent = "Desert";
+    document.querySelector(".swa").textContent = "Swamp";
+    document.querySelector(".for").textContent = "Forest";
+    document.querySelector(".mar").textContent = "Marsh";
+    document.querySelector(".riv").textContent = "River";
+    document.querySelector(".hill").textContent = "Hill";
+    document.querySelector(".bar").textContent = "Barren";
+
+    document.querySelector(".g020").textContent = "0 - 20 MBGL";
+    document.querySelector(".g2040").textContent = "20 - 40 MBGL";
+    document.querySelector(".g4060").textContent = "40 - 60 MBGL";
+    document.querySelector(".g6080").textContent = "60 - 80 MBGL";
+    document.querySelector(".g80100").textContent = "80 - 100 MBGL";
+    document.querySelector(".g100120").textContent = "100 - 120 MBGL";
+
+    document.querySelector(".construction").textContent =
+        "🚧 WEBSITE UNDER CONSTRUCTION 👷! 🚧";
+});
+
+document.querySelector(".hindi").addEventListener("click", function() {
+    document.querySelector(".how").textContent =
+        "पृथ्वी 1.0 . का उपयोग कैसे करें";
+
+    document.querySelector(".p1").textContent =
+        "पृथ्वी 1.0 एक स्वदेशी निर्मित प्रणाली है जो आपको विशिष्ट रूप से और सटीक रूप से सर्वश्रेष्ठ दोमट संरक्षण विधि की गणना करने में मदद करती है जो आपकी भूमि के लिए सबसे उपयुक्त है।";
+
+    document.querySelector(".p2").textContent =
+        "बस आवश्यक पैरामीटर दर्ज करें और पृथ्वी स्वचालित रूप से आपको कई तरीके सुझाएगा जिनका पालन आप अपनी भूमि में मिट्टी के कटाव को कम करने के लिए कर सकते हैं।";
+
+    document.querySelector(".english").classList.remove("active-page");
+    document.querySelector(".hindi").classList.add("active-page");
+    document.querySelector(".punjabi").classList.remove("active-page");
+
+    document.querySelector(".title").textContent = "पृथ्वी 1.0";
+    document.querySelector(".dropdownheader").textContent =
+        "कृपया उपयुक्त मापदंडों का चयन करें -";
+    document.querySelector(".state").textContent = "राज्य -";
+    document.querySelector(".region").textContent = "क्षेत्र -";
+    document.querySelector(".soil").textContent = "मिट्टी के प्रकार -";
+    document.querySelector(".rain").textContent = "वार्षिक वर्षा -";
+    document.querySelector(".terrain").textContent = "भू-भाग -";
+    document.querySelector(".groundWater").textContent =
+        "भूजल स्तर (जमीन स्तर से नीचे मीटर में (न्यूनतम - अधिकतम)) -";
+    document.querySelector(".submit").value = "जमा ";
+
+    document.querySelector(".pun2").textContent = "पंजाब";
+
+    document.querySelector(".pun").textContent = "पंजाब";
+    document.querySelector(".up").textContent = "उतार प्रदेश";
+    document.querySelector(".wb").textContent = "पश्चिम बंगाल";
+    document.querySelector(".guj").textContent = "गुजरात";
+    document.querySelector(".har").textContent = "हरयाणा";
+    document.querySelector(".mp").textContent = "मध्य प्रदेश";
+    document.querySelector(".assa").textContent = "असम";
+    document.querySelector(".ap").textContent = "आंध्र प्रदेश";
+    document.querySelector(".kar").textContent = "कर्नाटक";
+    document.querySelector(".chha").textContent = "छत्तीसगढ";
+
+    document.querySelector(".sandy").textContent = "रेतीली मिट्टी";
+    document.querySelector(".silt").textContent = "गाद मिट्टी";
+    document.querySelector(".clay").textContent = "चिकनी मिट्टी";
+    document.querySelector(".loamy").textContent = "बलुई मिट्टी";
+    document.querySelector(".red").textContent = "लाल मिट्टी";
+    document.querySelector(".black").textContent = "काली मिट्टी";
+    document.querySelector(".alluvial").textContent = "जलोढ़ मिट्टी";
+    document.querySelector(".peaty").textContent = "पीट मिट्टी";
+    document.querySelector(".saline").textContent = "लवणीय मिट्टी";
+    document.querySelector(".chalky").textContent = "चाकली मिट्टी";
+    document.querySelector(".other").textContent = "अन्य ...";
+
+    document.querySelector(".u20").textContent = "20 सेमी के नीचे";
+    document.querySelector(".n2040").textContent = "20 - 40 सेमी";
+    document.querySelector(".n4060").textContent = "40 - 60 सेमी";
+    document.querySelector(".n60100").textContent = "60 - 100 सेमी";
+    document.querySelector(".n100150").textContent = "100 - 150 सेमी";
+    document.querySelector(".n150200").textContent = "150 - 200 सेमी";
+    document.querySelector(".o200").textContent = "200 सेमी से अधिक";
+
+    document.querySelector(".plat").textContent = "पठार";
+    document.querySelector(".moun").textContent = "पर्वत";
+    document.querySelector(".pla").textContent = "मैदानों";
+    document.querySelector(".val").textContent = "घाटी";
+    document.querySelector(".tun").textContent = "टुंड्रा";
+    document.querySelector(".oas").textContent = "शाद्वल";
+    document.querySelector(".des").textContent = "रेगिस्तान";
+    document.querySelector(".swa").textContent = "दलदली कीचड़";
+    document.querySelector(".for").textContent = "जंगल";
+    document.querySelector(".mar").textContent = "दलदल";
+    document.querySelector(".riv").textContent = "नदी";
+    document.querySelector(".hill").textContent = "पहाड़";
+    document.querySelector(".bar").textContent = "बंजर";
+
+    document.querySelector(".g020").textContent = "0 - 20 एमबीजीएल";
+    document.querySelector(".g2040").textContent = "20 - 40 एमबीजीएल";
+    document.querySelector(".g4060").textContent = "40 - 60 एमबीजीएल";
+    document.querySelector(".g6080").textContent = "60 - 80 एमबीजीएल";
+    document.querySelector(".g80100").textContent = "80 - 100 एमबीजीएल";
+    document.querySelector(".g100120").textContent = "100 - 120 एमबीजीएल";
+
+    document.querySelector(".construction").textContent =
+        "🚧 वेबसाइट निर्माणाधीन 👷! 🚧";
+});
+
+document.querySelector(".punjabi").addEventListener("click", function() {
+    document.querySelector(".how").textContent =
+        "ਪ੍ਰਿਥਵੀ 1.0 ਦੀ ਵਰਤੋਂ ਕਿਵੇਂ ਕਰੀਏ";
+
+    document.querySelector(".p1").textContent =
+        "ਪ੍ਰਿਥਵੀ 1.0 ਇੱਕ ਸਵਦੇਸ਼ੀ ਬਣਾਇਆ ਗਿਆ ਸਿਸਟਮ ਹੈ ਜੋ ਤੁਹਾਨੂੰ ਸਭ ਤੋਂ ਵਧੀਆ ਲੋਮ ਕੰਜ਼ਰਵੇਸ਼ਨ ਵਿਧੀ ਦੀ ਵਿਲੱਖਣ ਅਤੇ ਸਹੀ ਢੰਗ ਨਾਲ ਗਣਨਾ ਕਰਨ ਵਿੱਚ ਮਦਦ ਕਰਦਾ ਹੈ ਜੋ ਤੁਹਾਡੀ ਜ਼ਮੀਨ ਲਈ ਸਭ ਤੋਂ ਵਧੀਆ ਹੈ।";
+
+    document.querySelector(".p2").textContent =
+        "ਬਸ ਲੋੜੀਂਦੇ ਮਾਪਦੰਡ ਦਰਜ ਕਰੋ ਅਤੇ ਪ੍ਰਿਥਵੀ ਆਪਣੇ ਆਪ ਤੁਹਾਨੂੰ ਕਈ ਤਰੀਕਿਆਂ ਦਾ ਸੁਝਾਅ ਦੇਵੇਗਾ ਜਿਨ੍ਹਾਂ ਦੀ ਤੁਸੀਂ ਆਪਣੀ ਜ਼ਮੀਨ ਵਿੱਚ ਮਿੱਟੀ ਦੇ ਕਟਾਵ ਨੂੰ ਘਟਾਉਣ ਲਈ ਅਪਣਾ ਸਕਦੇ ਹੋ।";
+
+    document.querySelector(".english").classList.remove("active-page");
+    document.querySelector(".hindi").classList.remove("active-page");
+    document.querySelector(".punjabi").classList.add("active-page");
+
+    document.querySelector(".title").textContent = "ਪ੍ਰਿਥਵੀ 1.0";
+    document.querySelector(".dropdownheader").textContent =
+        "ਕਿਰਪਾ ਕਰਕੇ ਉਚਿਤ ਮਾਪਦੰਡ ਚੁਣੋ -";
+    document.querySelector(".state").textContent = "ਰਾਜ -";
+    document.querySelector(".region").textContent = "ਖੇਤਰ -";
+    document.querySelector(".soil").textContent = "ਮਿੱਟੀ ਦੀਆਂ ਕਿਸਮਾਂ -";
+    document.querySelector(".rain").textContent = "ਸਾਲਾਨਾ ਵਰਖਾ -";
+    document.querySelector(".terrain").textContent = "ਭੂਮੀ -";
+    document.querySelector(".groundWater").textContent =
+        "ਜ਼ਮੀਨੀ ਪਾਣੀ ਦਾ ਪੱਧਰ (ਜ਼ਮੀਨੀ ਪੱਧਰ ਤੋਂ ਹੇਠਾਂ ਮੀਟਰਾਂ ਵਿੱਚ (ਘੱਟੋ-ਵੱਧ) -";
+    document.querySelector(".submit").value = "ਜਮ੍ਹਾਂ ਕਰੋ";
+
+    document.querySelector(".pun2").textContent = "ਪੰਜਾਬ";
+
+    document.querySelector(".pun").textContent = "ਪੰਜਾਬ";
+    document.querySelector(".up").textContent = "ਉੱਤਰ ਪ੍ਰਦੇਸ਼";
+    document.querySelector(".wb").textContent = "ਪੱਛਮੀ ਬੰਗਾਲ";
+    document.querySelector(".guj").textContent = "ਗੁਜਰਾਤ";
+    document.querySelector(".har").textContent = "ਹਰਿਆਣਾ";
+    document.querySelector(".mp").textContent = "ਮੱਧ ਪ੍ਰਦੇਸ਼";
+    document.querySelector(".assa").textContent = "ਅਸਾਮ";
+    document.querySelector(".ap").textContent = "ਆਂਧਰਾ ਪ੍ਰਦੇਸ਼";
+    document.querySelector(".kar").textContent = "ਕਰਨਾਟਕ";
+    document.querySelector(".chha").textContent = "ਛੱਤੀਸਗੜ੍ਹ";
+
+    document.querySelector(".sandy").textContent = "ਰੇਤਲੀ ਮਿੱਟੀ";
+    document.querySelector(".silt").textContent = "ਸਿਲਟ ਮਿੱਟੀ";
+    document.querySelector(".clay").textContent = "ਮਿੱਟੀ ਦੀ ਮਿੱਟੀ";
+    document.querySelector(".loamy").textContent = "ਲੋਮੀ ਮਿੱਟੀ";
+    document.querySelector(".red").textContent = "ਲਾਲ ਮਿੱਟੀ";
+    document.querySelector(".black").textContent = "ਕਾਲੀ ਮਿੱਟੀ";
+    document.querySelector(".alluvial").textContent = "ਐਲੂਵੀਅਲ ਮਿੱਟੀ";
+    document.querySelector(".peaty").textContent = "ਪੀਟੀ ਮਿੱਟੀ";
+    document.querySelector(".saline").textContent = "ਖਾਰੀ ਮਿੱਟੀ";
+    document.querySelector(".chalky").textContent = "ਚੱਕੀ ਮਿੱਟੀ";
+    document.querySelector(".other").textContent = "ਹੋਰ...";
+
+    document.querySelector(".u20").textContent = "20 ਸੈ.ਮੀ";
+    document.querySelector(".n2040").textContent = "20 - 40 ਸੈ.ਮੀ";
+    document.querySelector(".n4060").textContent = "40 - 60 ਸੈ.ਮੀ";
+    document.querySelector(".n60100").textContent = "60 - 100 ਸੈ.ਮੀ";
+    document.querySelector(".n100150").textContent = "100 - 150 ਸੈ.ਮੀ";
+    document.querySelector(".n150200").textContent = "150 - 200 ਸੈ.ਮੀ";
+    document.querySelector(".o200").textContent = "200 ਸੈਂਟੀਮੀਟਰ ਤੋਂ ਵੱਧ";
+
+    document.querySelector(".plat").textContent = "ਪਠਾਰ";
+    document.querySelector(".moun").textContent = "ਪਹਾੜ";
+    document.querySelector(".pla").textContent = "ਮੈਦਾਨੀ";
+    document.querySelector(".val").textContent = "ਵਾਦੀ";
+    document.querySelector(".tun").textContent = "ਟੁੰਡਰਾ";
+    document.querySelector(".oas").textContent = "ਓਏਸਿਸ";
+    document.querySelector(".des").textContent = "ਮਾਰੂਥਲ";
+    document.querySelector(".swa").textContent = "ਦਲਦਲ";
+    document.querySelector(".for").textContent = "ਜੰਗਲ";
+    document.querySelector(".mar").textContent = "ਮਾਰਸ਼";
+    document.querySelector(".riv").textContent = "ਨਦੀ";
+    document.querySelector(".hill").textContent = "ਪਹਾੜੀ";
+    document.querySelector(".bar").textContent = "ਬੰਜਰ";
+
+    document.querySelector(".g020").textContent = "0 - 20 MBGL";
+    document.querySelector(".g2040").textContent = "20 - 40 MBGL";
+    document.querySelector(".g4060").textContent = "40 - 60 MBGL";
+    document.querySelector(".g6080").textContent = "60 - 80 MBGL";
+    document.querySelector(".g80100").textContent = "80 - 100 MBGL";
+    document.querySelector(".g100120").textContent = "100 - 120 MBGL";
+
+    document.querySelector(".construction").textContent =
+        "🚧 ਵੈੱਬਸਾਈਟ ਨਿਰਮਾਣ ਅਧੀਨ ਹੈ 👷! 🚧";
+});
